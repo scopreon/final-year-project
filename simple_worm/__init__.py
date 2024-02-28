32c5512fb2e740ec775543d15a696780e989e943
Saul Cooperman <sc21sc@leeds.ac.uk>
2024-02-28 10:07:59 +0000

    Add path function

--- simple_worm/__init__.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-from fenics import *
-from ffc.quadrature.deprecation import QuadratureRepresentationDeprecationWarning
-
-set_log_level(100)
-warnings.simplefilter('ignore', QuadratureRepresentationDeprecationWarning)
